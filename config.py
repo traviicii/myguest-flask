2e845509275a00fb341aa20aadc65cd0691df360
traviicii <125103256+traviicii@users.noreply.github.com>
2023-05-28 11:49:26 -0400

    Attempting to correct google auth login on hosted site in config and env files

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config():
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_APPLICATION_CREDENTIALS = os.path.abspath(os.path.dirname("client-keeper-a2e91-firebase-adminsdk-tvdur-917c3f59a9"))
+    GOOGLE_APPLICATION_CREDENTIALS = os.path.abspath("client-keeper-a2e91-firebase-adminsdk-tvdur-917c3f59a9.json")
 
-temp = 'GOOGLE_APPLICATION_CREDENTIALS'
+temp = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
